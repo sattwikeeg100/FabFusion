94a7016482b10e8f1317f51d5f2faff9176f0fab
Sattwikee Ghosh <sattwikeeghosh95@gmail.com>
2024-04-07 20:31:32 +0530

    sorting products

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -66,8 +66,8 @@ const getaProduct = asyncHandler(async (req, res) => {
 
 const getAllProducts = asyncHandler(async (req, res) => {
 
-    // Filtering Products
     try{
+        // Filtering
         const queryObj = { ...req.query };
         const excludeFields = ['page', 'sort', 'limit', 'fields'];
         excludeFields.forEach((el) => delete queryObj[el]);
@@ -75,7 +75,16 @@ const getAllProducts = asyncHandler(async (req, res) => {
         let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
         
-        const query = Product.find(JSON.parse(queryStr));
+        let query = Product.find(JSON.parse(queryStr));
+
+        // Sorting
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt');
+        }
+
         const products = await query;
         res.json(products);
     } catch(error) {
