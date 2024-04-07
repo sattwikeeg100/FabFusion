82845cd48fea8a79cce1e33778604b67fbd584eb
Sattwikee Ghosh <sattwikeeghosh95@gmail.com>
2024-04-07 20:51:10 +0530

    limiting fields and pagination

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -85,6 +85,26 @@ const getAllProducts = asyncHandler(async (req, res) => {
             query = query.sort('-createdAt');
         }
 
+        // limiting the fields
+
+        if (req.query.fields) {
+            const fields = req.query.fields.split(",").join(" ");
+            query = query.select(fields);
+        } else {
+            query = query.select("-__v");
+        }
+
+        // pagination
+
+        const page = req.query.page;
+        const limit = req.query.limit;
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit);
+        if (req.query.page) {
+            const productCount = await Product.countDocuments();
+            if (skip >= productCount) throw new Error("This Page does not exists");
+        }
+
         const products = await query;
         res.json(products);
     } catch(error) {
